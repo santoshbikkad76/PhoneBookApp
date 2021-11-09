095344fb84fda1db7445c5abf16424a08e819cdd
GanRaaJ <you@example.com>
2021-11-09 21:03:14 +0530

    PHON-1 : Project Structure created

---
+++ src/main/java/com/bikkadIt/PhoneBookApplication1Application.java
@@ -0,0 +1,13 @@
+package com.bikkadIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookApplication1Application.class, args);
+	}
+
+}
