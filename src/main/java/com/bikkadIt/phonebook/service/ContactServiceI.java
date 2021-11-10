975e688d50da4e1adffc4270f31a8e5a9f08e2bd
GanRaaJ <you@example.com>
2021-11-10 21:48:05 +0530

    PHON-5 :Implementation of service layer

--- src/main/java/com/bikkadIt/phonebook/service/ContactServiceI.java
+++ src/main/java/com/bikkadIt/phonebook/service/ContactServiceI.java
@@ -1,5 +1,17 @@
 package com.bikkadIt.phonebook.service;
 
+import java.util.List;
+
+import com.bikkadIt.phonebook.entity.Contact;
+
 public interface ContactServiceI {
+
+	boolean saveContact(Contact contact);
+
+	List<Contact> getAllContacts( );
+
+	Contact getContactById(Integer cid);
+
+	boolean deleteContactById(Integer cid);
 
 }
--- src/main/java/com/bikkadIt/phonebook/service/ContactServiceImpl.java
+++ src/main/java/com/bikkadIt/phonebook/service/ContactServiceImpl.java
@@ -1,8 +1,55 @@
 package com.bikkadIt.phonebook.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.bikkadIt.phonebook.entity.Contact;
+import com.bikkadIt.phonebook.repository.ContactRepository;
 
 @Service
 public class ContactServiceImpl implements ContactServiceI {
+
+	@Autowired
+	private ContactRepository contactRepository;
+	
+	@Override
+	public boolean saveContact(Contact contact) {
+		Contact save=contactRepository.save(contact);
+		if(save !=null && save.getContactId() != null) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<Contact> getAllContacts() {
+		List<Contact> findAll = contactRepository.findAll();
+		return findAll;
+	}
+
+	@Override
+	public Contact getContactById(Integer cid) {
+		
+		Optional<Contact> findById = contactRepository.findById(cid);
+		if(findById.isPresent()) {
+		Contact contact=	findById.get();
+		return contact;
+		}
+		return null;
+	}
+
+	@Override
+	public boolean deleteContactById(Integer cid) {
+
+		boolean status=	contactRepository.existsById(cid);
+		if(status) {
+		contactRepository.deleteById(cid);
+		return true;
+		}
+		return false;
+	}
 
 }
