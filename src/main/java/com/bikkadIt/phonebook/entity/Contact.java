23a93f6507f85face5662c006382e5fa6f706253
GanRaaJ <you@example.com>
2021-11-10 21:14:35 +0530

    PHON-4 :create Entiry Class

--- src/main/java/com/bikkadIt/phonebook/entity/Contact.java
+++ src/main/java/com/bikkadIt/phonebook/entity/Contact.java
@@ -1,5 +1,105 @@
 package com.bikkadIt.phonebook.entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CONTACT_DTLS")
 public class Contact {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="CONTACT_ID")
+	private Integer contactId;
+	
+	@Column(name="CONTACT_NAME")
+	private String contactName;
+	
+	@Column(name="CONTACT_NUMBER")
+	private Long contactNumber;
+	
+	@Column(name="CONTACT_EMAIL")
+	private String contactEmail;
+	
+	@Column(name="ACTIVE_SW")
+	private Character activeSw;
+	
+	@Column(name="CREATED_DATE")
+	private LocalDate createdDate;
+	
+	@Column(name="UPDATED_DATE")
+	private LocalDate updatedDate;
+
+	public Integer getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Integer contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public Long getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(Long contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getContactEmail() {
+		return contactEmail;
+	}
+
+	public void setContactEmail(String contactEmail) {
+		this.contactEmail = contactEmail;
+	}
+
+	public Character getActiveSw() {
+		return activeSw;
+	}
+
+	public void setActiveSw(Character activeSw) {
+		this.activeSw = activeSw;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDate getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDate updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [contactId=" + contactId + ", contactName=" + contactName + ", contactNumber=" + contactNumber
+				+ ", contactEmail=" + contactEmail + ", activeSw=" + activeSw + ", createdDate=" + createdDate
+				+ ", updatedDate=" + updatedDate + "]";
+	}
+	
+	
+	
+	
 }
--- src/main/java/com/bikkadIt/phonebook/repository/ContactRepository.java
+++ src/main/java/com/bikkadIt/phonebook/repository/ContactRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.bikkadIt.phonebook.entity.Contact;
+
+
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Serializable>  {
 
